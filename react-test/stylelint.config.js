02a638e7303b80c628aa4d5c2f3e6d94bd60b6cc
jrojas <jrojas@hugeinc.com>
2021-05-21 15:22:29 -0500

    add initial project configuration with eslint, typescript, prettier, stylelint and some npm basic commmands to verify static code test

---
+++ react-test/stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['stylelint-order'],
+  rules: {
+    indentation: 2,
+    'property-no-unknown': true,
+    'order/properties-alphabetical-order': true,
+    'declaration-block-trailing-semicolon': 'always',
+  },
+};
